f587c0f700d7072401dab2e04ca1e0a41b7a8a0c
ahphand-tamu <143048460+ahphand-tamu@users.noreply.github.com>
2023-09-23 22:04:06 +0000

    more movies added

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,13 @@ more_movies = [
   {:title => 'Nomadland', :rating => 'R',
     :release_date => '19-Feb-2021'},
   {:title => 'CODA', :rating => 'PG-13',
-    :release_date => '13-Aug-2021'}
+    :release_date => '13-Aug-2021'},
+  {:title => 'Avengers Endgame', :rating => '10/10',
+    :release_date => '26-Apr-2019'},
+  {:title => 'Spiderman: No Way Home', :rating => 'PG-13',
+    :release_date => '17-Dev-2021'},
+  {:title => 'Interstellar', :rating => 'PG-13',
+    :release_date => '26-Oct-2014'}
 ]
 
 more_movies.each do |movie|
